1f456fa93e315e8f48eb7b57ef7428999fc0ba34
moz <moz@local>
2016-09-15 16:05:24 +0200

    doc stuff and some niceness

--- connect.py
+++ connect.py
@@ -1,15 +1,16 @@
 import requests
-#import json
 
 server = "http://localhost:5000"
 
+# get list of GPIOs
 url = "%s/gpio"%(server, )
 print "Getting from", url
 r = requests.get(url)
 print "- return code", r.status_code
-print "-", r.json()
+print "- data:", r.json()
 print ""
 
+# get value for A0
 url = "%s/gpio/A0"%(server, )
 print "Getting from", url
 r = requests.get(url)
@@ -18,13 +19,14 @@ print "- return code", r.status_code
 print "- data:", getData
 print ""
 
+# set (using put) value for A0
 putData = {'data': str(int(getData['value'])+2) }
-#putDataJson = json.dumps( putData )
 print "Putting data", putData, "to", url
 r = requests.put(url, json=putData)
 print "- return code", r.status_code
 print ""
 
+# get newvalue for A0
 print "Getting from", url
 r = requests.get(url)
 getData = r.json()
