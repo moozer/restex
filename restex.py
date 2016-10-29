ff2f2e3f2a6bda75524ceb2e99e0c3f584a5a67a
moz <moz@local>
2016-10-30 00:22:25 +0200

    fixed values vs. data naming bug

--- restex.py
+++ restex.py
@@ -3,10 +3,10 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app, resources=r'/*') # to allow external resources to fetch data 
+CORS(app, resources=r'/*') # to allow external resources to fetch data
 api = Api(app)
 
-gpios = {"A0": { 'value': 0}, 
+gpios = {"A0": { 'value': 0},
          "A1": { 'value': 0},
          "B0": { 'value': 0} }
 
@@ -32,11 +32,10 @@ class url_gpio_name( Resource ):
 
     def put( self, gpio_name ):
         try:
-            gpios[ gpio_name ]['value'] = request.json['data']
+            gpios[ gpio_name ]['value'] = request.json['value']
             return "", 204
         except KeyError, ex:
             return { "message": "key error: Either '%s' is not a GPIO name or data is undefined (received: '%s')"%(gpio_name, str(request.form)) }, 400
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
-
